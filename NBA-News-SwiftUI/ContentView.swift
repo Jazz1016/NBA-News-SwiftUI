4895fc38cc2cb5b5bdbbe46a208032d2866c1675
James Lea <james.q.lea@gmail.com>
2023-05-12 10:39:03 -0600

    first commit

---
+++ NBA-News-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NBA-News-SwiftUI
+//
+//  Created by James Lea on 5/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NBA-News-SwiftUI/NBA_News_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBA_News_SwiftUIApp.swift
+//  NBA-News-SwiftUI
+//
+//  Created by James Lea on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct NBA_News_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
